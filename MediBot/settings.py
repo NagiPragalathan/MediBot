34e31eb72fc6478b09ab1914adb01e14f81723f2
nagipragalathan <kk2069037@gmail.com>
2023-05-25 02:07:20 +0530

    STATICFILES_DIRS are corrected in settings.py

--- MediBot/settings.py
+++ MediBot/settings.py
@@ -120,15 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
