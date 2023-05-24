cdcb90958a1fc6344a7a46f8f6ebc21d82dce534
nagipragalathan <kk2069037@gmail.com>
2023-05-25 02:03:34 +0530

    last commit

--- MediBot/settings.py
+++ MediBot/settings.py
@@ -83,6 +83,8 @@ WSGI_APPLICATION = 'MediBot.wsgi.application'
 #     }
 # }
 
+DATABASES = {}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
