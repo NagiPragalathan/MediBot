9356788790563eb91543c8fd0400e3cb33430176
NagiPragalathan <83810241+NagiPragalathan@users.noreply.github.com>
2023-07-26 12:09:48 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,12 +1,3 @@
-let count=0;
-console.log("working..");
-function change_date(){
-  console.log("clicked..");
-  let obj = document.getElementById('demo');
-  if(count%2==0){
-    obj.innerHTML = Date();
-  }else{
-    obj.innerHTML = "Click the button to show the Date";
-  }
-  count++;
+function myFunction() {
+    document.getElementById("demo").innerHTML="Paragraph changed.";
 }
